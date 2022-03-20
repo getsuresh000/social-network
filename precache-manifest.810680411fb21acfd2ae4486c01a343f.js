b0efb8a381ebd75eae8abdc88737cce946daa718
getsuresh000 <getsuresh000@gmail.com>
2022-03-20 15:23:33 +0530

    Updates

---
+++ precache-manifest.810680411fb21acfd2ae4486c01a343f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f537408d861434944d45",
+    "url": "/social-network/static/css/main.3df78935.chunk.css"
+  },
+  {
+    "revision": "f537408d861434944d45",
+    "url": "/social-network/static/js/main.f537408d.chunk.js"
+  },
+  {
+    "revision": "ea25003dd6ef0728cc9d",
+    "url": "/social-network/static/css/1.e5c9c3d7.chunk.css"
+  },
+  {
+    "revision": "ea25003dd6ef0728cc9d",
+    "url": "/social-network/static/js/1.ea25003d.chunk.js"
+  },
+  {
+    "revision": "22e660ce69f7e2ad833f",
+    "url": "/social-network/static/js/runtime~main.22e660ce.js"
+  },
+  {
+    "revision": "13ae2b3ac835b2d5451897f1b3646bc6",
+    "url": "/social-network/index.html"
+  }
+];
